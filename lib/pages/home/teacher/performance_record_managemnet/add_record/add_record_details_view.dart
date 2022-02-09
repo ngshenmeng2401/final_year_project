3fd7590bd22c925e77eb0d528a533dcf1f4acf8d
NG SHEN MENG <shenmeng0124@gmail.com>
2022-02-09 23:43:40 +0800

    edit

---
+++ lib/pages/home/teacher/performance_record_managemnet/add_record/add_record_details_view.dart
@@ -0,0 +1,92 @@
+import 'package:final_year_project/pages/home/teacher/performance_record_managemnet/add_record/listening_questions.dart';
+import 'package:final_year_project/pages/home/teacher/performance_record_managemnet/add_record/reading_questions.dart';
+import 'package:final_year_project/pages/home/teacher/performance_record_managemnet/add_record/speaking_questions.dart';
+import 'package:final_year_project/pages/home/teacher/performance_record_managemnet/add_record/writing_questions.dart';
+import 'package:final_year_project/pages/home/teacher/performance_record_managemnet/performance_record_controller.dart';
+import 'package:final_year_project/pages/home/teacher/performance_record_managemnet/performance_score_table.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AddRecordDetailPage extends GetView<PerformanceRecordController> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Record Detail"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              PerformanceScoreTable(),
+              SizedBox(height: screenHeight/30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Student ID",
+                  style: TextStyle(fontSize: 22),),
+                  GetBuilder<PerformanceRecordController>(
+                    init: PerformanceRecordController(),
+                    builder: (_) {
+                      return Container(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: DropdownButton<String?>(
+                          // dropdownColor: Colors.blue,
+                          menuMaxHeight: screenHeight/4,
+                          value: controller.selectName,
+                          elevation: 28,
+                          style: const TextStyle(fontSize: 20),
+                          onChanged: (String? newValue) {
+                            controller.chooseStudent(newValue!);
+                          },
+                          items: [
+                            for (var data in controller.studentName)
+                              DropdownMenuItem(
+                                child: new Text(
+                                  data,
+                                ),
+                                value: data,
+                              )
+                          ]
+                        ));
+                    },
+                  ),
+                ],
+              ),
+              Divider(),
+              ListeningQuestions(),
+              Divider(),
+              SpeakingQuestions(),
+              Divider(),
+              ReadingQuestions(),
+              Divider(),
+              WritingQuestions(),
+              Divider(),
+              MaterialButton(
+                shape:RoundedRectangleBorder(
+                  borderRadius:BorderRadius.circular(20),
+                ),
+                minWidth: screenWidth/1,
+                height: screenHeight/18,
+                child: Text('Submit',
+                  style: TextStyle(fontSize: 20,),
+                ),
+                onPressed: (){
+                  
+                },
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widget/bottom_navigation_binding.dart
+++ lib/widget/bottom_navigation_binding.dart
@@ -1,4 +1,3 @@
-import 'package:final_year_project/pages/home/home_controller.dart';
 import 'package:final_year_project/pages/profile/profile_controller.dart';
 import 'package:final_year_project/widget/bottom_navigation_bar_controller.dart';
 import 'package:get/get.dart';
@@ -7,7 +6,6 @@ class BottomNavigationBarBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<BottomNavigationBarController>(() => BottomNavigationBarController());
-    Get.lazyPut<HomeController>(() => HomeController());
     Get.lazyPut<ProfileController>(() => ProfileController());
     // Get.lazyPut<SearchController>(() => SearchController());
   }
